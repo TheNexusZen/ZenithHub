e939247a65d9eeef18ff2b5fe43a0a0723bec99e
NexusZenLmao <nexzenomg@gmail.com>
2026-03-07 18:56:56 +0300

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,39 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+WindUI:AddTheme({
+    Name = "Blue", -- theme name
+    
+    Accent = Color3.fromHex("#18181b"),
+    Background = Color3.fromHex("#101010"), -- Accent
+    Outline = Color3.fromHex("#FFFFFF"),
+    Text = Color3.fromHex("#FFFFFF"),
+    Placeholder = Color3.fromHex("#7a7a7a"),
+    Button = Color3.fromHex("#52525b"),
+    Icon = Color3.fromHex("#a1a1aa"),
+})
+
+WindUI:SetTheme("Blue")
+
+local Window = WindUI:CreateWindow({
+    Title = "Zenith HUB",
+    Icon = "app-window-mac",
+    Author = "by NEXUSZENITH",
+    Folder = "ZenithHub",
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    Transparent = true,
+    Theme = "Blue",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = false,
+    ScrollBarEnabled = false,
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+            Anonymous = not Anonymous
+        end,
+    },
+})
